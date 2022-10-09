38299f965f726d4af7e9e8660c5eed03b8d5ad1e
Brashan <brashanm@gmail.com>
2022-10-09 12:30:14 -0400

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to TTD Kicks</h1>
+      <Header />
+      <main className='py-2'></main>
+        <Container>
+          <h1>Welcome to TTD Kicks</h1>
+        </Container>
+      <Footer />
     </>
   );
 }
