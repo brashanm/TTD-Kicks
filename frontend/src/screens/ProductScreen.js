d67801aa37ba13672e896b0ca19ecf7d01f6830e
Brashan <brashanm@gmail.com>
2022-10-11 17:32:08 -0400

    Commit

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
+    const [product, setProduct] = useState({})
     const { id } = useParams()
-    const product = products.find((item) => item._id === id)
+    useEffect(() => {
+        const fetchProduct = async () => {
+          const { data } = await axios.get(`/api/products/${id}`)
+          setProduct(data)
+        }
+        fetchProduct()
+      }, [])
   return (
     <>
         <Link className='btn btn-light my-3' to='/'>Go Back</Link>
