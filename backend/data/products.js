d45d21ce82d0f1eccb6b311a1caa7226d04af55d
Brashan <brashanm@gmail.com>
2022-10-11 00:03:39 -0400

    Adding Backend

---
+++ backend/data/products.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Air Jordan 4 Retro Military Black GS',
+        image: '/TTD Kicks Shoe Photos/Air Jordan 4 Retro Military Black GS.png',
+        brand: 'Jordan',
+        prices: 450,
+        stock: 3,
+        rating: 4.5,
+        numReviews: 4,
+    },
+    {
+        _id: '2',
+        name: 'Jordan 1 Low OG UNC',
+        image: '/TTD Kicks Shoe Photos/Jordan 1 Low OG UNC.png',
+        brand: 'Jordan',
+        prices: 280,
+        stock: 2,
+        rating: 4,
+        numReviews: 3,
+    },
+    {
+        _id: '3',
+        name: 'Nike Dunk Low Lottery Pack Grey Fog',
+        image: '/TTD Kicks Shoe Photos/Nike Dunk Low Lottery Pack Grey Fog.png',
+        brand: 'Nike',
+        prices: 250,
+        stock: 3,
+        rating: 4.25,
+        numReviews: 4,
+    },
+    {
+        _id: '4',
+        name: 'Nike Dunk Low Lottery Pack Malachite Green',
+        image: '/TTD Kicks Shoe Photos/Nike Dunk Low Lottery Pack Malachite Green.png',
+        brand: 'Nike',
+        prices: 260,
+        stock: 3,
+        rating: 4.5,
+        numReviews: 4,
+    },
+]
+
+export default products
