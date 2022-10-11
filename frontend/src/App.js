ac84762cb37f046bc44ccc9bfc421d691a345986
Brashan <brashanm@gmail.com>
2022-10-10 22:36:42 -0400

    Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react'
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header'
+import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
-import Homescreen from './screens/Homescreen';
+import Homescreen from './screens/Homescreen'
+import ProductScreen from './screens/ProductScreen'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <main className='py-2'></main>
-        <Container>
-          <Homescreen />
-        </Container>
+        <main className='py-2'></main>
+          <Container>
+            <Routes>
+              <Route path='/' element={<Homescreen />} exact/>
+              <Route path='/product/:id' element={<ProductScreen />} />
+            </Routes>
+          </Container>
       <Footer />
-    </>
+    </Router>
   );
 }
 
